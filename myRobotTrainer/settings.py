4484630baff3edb9f92667385e72263cd0217342
Joshua Schneider <68866456+VinnyTheYahtzee@users.noreply.github.com>
2025-01-30 21:15:47 +0100

    added procfile, adjusted settings in settings.py, added gitignore for .env

--- myRobotTrainer/settings.py
+++ myRobotTrainer/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 
 from pathlib import Path
 from decouple import config
+import dj_database_url
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -26,7 +28,7 @@ SECRET_KEY = 'django-insecure-vo_m-nn3^1oyk9$rcat#0*nv!ysok-(^^h2wh)p0a@s$kj&q7@
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['https://webtech-backend-092e.onrender.com']
 
 
 # Application definition
@@ -108,12 +110,14 @@ REST_FRAMEWORK = {
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    'default': { dj_database_url.config(default=os.getenv('DATABASE_URL'))
+        
     }
 }
 
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+MEDIA_URL = "/media/"
+MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
