e3db43a98870d56a561170e0ab9517e32eca130c
Joshua Schneider <68866456+VinnyTheYahtzee@users.noreply.github.com>
2025-01-30 21:27:28 +0100

    fixed settings.py

--- myRobotTrainer/settings.py
+++ myRobotTrainer/settings.py
@@ -110,9 +110,7 @@ REST_FRAMEWORK = {
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
-    'default': { dj_database_url.config(default=os.getenv('DATABASE_URL'))
-        
-    }
+    'default': dj_database_url.config(default=os.getenv('DATABASE_URL'))
 }
 
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
